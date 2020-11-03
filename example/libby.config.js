fda3d3ae91bf65d0365603f7997e74c0c55d4058
Jon Ambas <jon@jonambas.com>
2020-11-03 13:52:09 -0500

    Use published version in example

--- example/libby.config.js
+++ example/libby.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   entries: () => require.context('./src', true, /\.libby\.js$/),
   title: 'Libby Example',
-  webpackConfig: () => ({}),
   layout: '.libby/layout.js',
   port: 9003,
-  outputPath: 'libby-example'
+  outputPath: 'dist/libby-example'
 };
