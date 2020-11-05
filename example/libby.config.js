ba2945986b2ee2cdcdf6fe1a9c1b1430f8ab07c8
Jon Ambas <jon@jonambas.com>
2020-11-04 20:47:35 -0500

    Add example backgrounds

--- example/libby.config.js
+++ example/libby.config.js
@@ -3,5 +3,22 @@ module.exports = {
   title: 'Libby Example',
   layout: '.libby/layout.js',
   port: 9003,
-  outputPath: 'dist/libby-example'
+  outputPath: 'dist/libby-example',
+  backgrounds: {
+    default: 'white',
+    values: [
+      {
+        name: 'white',
+        value: '#ffffff'
+      },
+      {
+        name: 'pink',
+        value: '#FFCCD5'
+      },
+      {
+        name: 'gray',
+        value: '#ebf0f5'
+      }
+    ]
+  }
 };
