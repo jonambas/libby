d708f649f06126a7cf8986b5fefeea876b78d78c
Jon Ambas <jon@jonambas.com>
2021-02-01 13:23:49 -0500

    Replace error overlay plugin

--- src/preview/source.js
+++ src/preview/source.js
@@ -3,9 +3,17 @@ import reactElementToJSXString from 'react-element-to-jsx-string';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { ghcolors } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
+import { startReportingRuntimeErrors, stopReportingRuntimeErrors } from 'react-error-overlay';
 
 function Source(props) {
   SyntaxHighlighter.registerLanguage('jsx', jsx);
+
+  React.useEffect(() => {
+    startReportingRuntimeErrors({ onError: () => {} });
+    return () => {
+      stopReportingRuntimeErrors();
+    };
+  }, []);
 
   return (
     <div id="libby-source">
